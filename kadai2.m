3ae245e1837627f034efcddfa6662bfe91fc5f2d
武田遼太 <ryota522@ezweb.ne.jp>
2019-12-07 14:07:28 +0900

    Update kadai2.m

--- kadai2.m
+++ kadai2.m
@@ -21,6 +21,10 @@ IMG1 = ORG>128;
 IMG2 = ORG>192;
 IMG = IMG0 + IMG1 + IMG2;
 imagesc(IMG); colormap(gray); colorbar;  axis image;
+pause;
 
 % ８階調については，各自検討してください．
-IMG3
+IMG3 = ORG>256;
+IMG = IMG3;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+pause;
